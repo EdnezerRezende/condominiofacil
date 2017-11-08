81ae658c5fec8d90ac8457e03b4711f164590b0b
egrezende <egrezende@stefanini.com>
2017-11-08 10:29:58 -0200

    Atualizar Banch Master

--- app/infra/connectionFactory.js
+++ app/infra/connectionFactory.js
@@ -1,6 +1,18 @@
 var mysql = require('mysql');
 
 function createDBConnection(){
+	if (process.env.NODE_ENV == 'production'){
+		return mysql.createConnection({
+			host: 'us-cdbr-iron-east-05.cleardb.net',
+			user: 'be3010b49d1863',
+			password: 'c725247b',
+			database: 'heroku_6d0ba34dd13df69',
+		    headers: {
+				'Accept':'application/json',
+				'Content-type':'application/json'
+			}
+		})
+	}
 	return mysql.createConnection({
 		host: 'localhost',
 		port: 3306,
