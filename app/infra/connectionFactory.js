d6ccbabd83e46f563339afdab8e39609fe1e7a84
egrezende <egrezende@stefanini.com>
2017-11-09 09:08:32 -0200

    Alterações de Timeout

--- app/infra/connectionFactory.js
+++ app/infra/connectionFactory.js
@@ -1,13 +1,12 @@
 var mysql = require('mysql');
-//var pool = null;
+
 function createDBConnection(){
 	if (process.env.NODE_ENV == 'production'){
 		return mysql.createConnection({
 			host: 'us-cdbr-iron-east-05.cleardb.net',
 			user: 'be3010b49d1863',
 			password: 'c725247b',
-			connectionLimit: 10,
-			acquireTimeout: 1000000,
+			connectTimeout: 100000,
 			database: 'heroku_6d0ba34dd13df69',
 		    headers: {
 				'Accept':'application/json',
@@ -21,8 +20,7 @@ function createDBConnection(){
 			port: 3306,
 			user: 'root',
 			password: 'admin',
-			connectionLimit: 10,
-			acquireTimeout: 1000000,
+			connectTimeout: 100000,
 			database: 'condominiofacil',
 		    headers: {
 				'Accept':'application/json',
